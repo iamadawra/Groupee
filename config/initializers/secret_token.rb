1f3255ffc3dc560d0daa719861e7c93e18d71c5d
iamadawra <aayushgopaldawra@gmail.com>
2015-07-17 20:21:37 -0700

    Base Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupee::Application.config.secret_key_base = '7414418f873629b2e1c21db37921254d47bc461c4d9f90dd08d315948ca64a1fcbd29ba08a12701483b35633cd31b7e2adfd7f986562215a5fef969804ec807b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groupee::Application.config.session_store :cookie_store, key: '_groupee_session'
