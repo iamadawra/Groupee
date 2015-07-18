4ef3308f84571458cb7599559ae53053520d0439
Alex Liow <gll34@cornell.edu>
2015-07-18 02:03:32 -0700

    Integrated the views and error404

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  get 'error/error404'
+
   devise_for :users
   resources :users
+
+  # Errors
+  get "error/404"
+
+  match "/404" => "error#error404", via: [ :get, :post, :patch, :delete ]
+  root :to => "static#index"
 end
